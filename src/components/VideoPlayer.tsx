2cb7a041ea1a9cf9e11bcc98291226ea3b138a41
Pegaiur <pegaiur@gmail.com>
2021-04-19 16:58:25 +0800

    step forward/back

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,7 +1,57 @@
 import React from 'react';
 
-interface IState {}
+interface IState {
+  paused: boolean;
+}
 
-interface IProps {}
+interface IProps {
+  videoPath: string;
+}
 
-export default class VideoPlayer extends React.Component<IProps, IState> {}
+export default class VideoPlayer extends React.Component<IProps, IState> {
+  player: React.RefObject<HTMLVideoElement>;
+
+  constructor(props: IProps, state: IState) {
+    super(props, state);
+
+    this.state = {
+      paused: true,
+    };
+
+    this.stepForward = this.stepForward.bind(this);
+    this.switchPlaying = this.switchPlaying.bind(this);
+    this.player = React.createRef();
+  }
+
+  stepForward(step: number) {
+    this.player.current!.currentTime += step;
+  }
+
+  switchPlaying() {
+    if (this.state.paused) {
+      this.player.current?.play();
+    } else {
+      this.player.current?.pause();
+    }
+    this.setState({ paused: !this.state.paused });
+  }
+
+  render() {
+    return (
+      <div>
+        <video src={this.props.videoPath} ref={this.player} />
+        <div>
+          <button onClick={(_) => this.stepForward(-0.1)}>⏪ 0.1</button>
+          <button onClick={(_) => this.stepForward(-0.3)}>⏪ 0.3</button>
+          <button onClick={(_) => this.stepForward(-0.5)}>⏪ 0.5</button>
+          <button onClick={(_) => this.switchPlaying()}>
+            {this.state.paused ? '▶️' : '⏸️'}
+          </button>
+          <button onClick={(_) => this.stepForward(0.1)}>⏩ 0.1</button>
+          <button onClick={(_) => this.stepForward(0.3)}>⏩ 0.3</button>
+          <button onClick={(_) => this.stepForward(0.5)}>⏩ 0.5</button>
+        </div>
+      </div>
+    );
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,37 @@
+import VideoPlayer from '@/components/VideoPlayer';
 import React from 'react';
 import styles from './index.less';
 
-interface IState {}
-
 interface IProps {}
 
+interface IState {
+  videoPath: string;
+}
+
 export default class IndexPage extends React.Component<IProps, IState> {
+  constructor(state: IState) {
+    super(state);
+
+    this.state = {
+      videoPath: '',
+    };
+  }
+
   render() {
     return (
       <div>
         <h1 className={styles.title}>资源准备页</h1>
+        源视频:
+        <input
+          type="file"
+          onChange={(event) => {
+            let firstItem = event.currentTarget.files?.item(0);
+            this.setState({ videoPath: URL.createObjectURL(firstItem) });
+          }}
+        />
+        {this.state.videoPath == '' ? null : (
+          <VideoPlayer videoPath={this.state.videoPath} />
+        )}
       </div>
     );
   }
